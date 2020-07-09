77892d2f583f5d3b84670878acdc95edde605e5b
Johnny Ruiz <jeko2000@yandex.com>
2020-07-09 17:02:55 -0400

    Complete 47. Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Mr. Robot'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Mr. Robot'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        helpText: 'This is a sample help message'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -14,6 +36,7 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/')
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
