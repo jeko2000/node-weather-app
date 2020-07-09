f9d8510deeb4696cfd9ef1399f26504ca18115f2
Johnny Ruiz <jeko2000@yandex.com>
2020-07-09 12:33:55 -0400

    Complete 44. Serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -3,19 +3,29 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([
+        {
+            name: 'Sample'
+        },
+        {
+            name: 'Sample2'
+        }
+    ])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forecast: 50,
+        location: 'Philadelphia'
+    })
 })
 
 app.listen(3000, () => {
