6fb4998a8fb0e15ed126d0b4d6ce6553616f7aeb
Johnny Ruiz <jeko2000@yandex.com>
2020-07-09 18:15:04 -0400

    Complete 49. Advanced Templating

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory
 app.use(express.static(publicDirectoryPath))
@@ -31,18 +34,19 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help page',
-        helpText: 'This is a sample help message'
+        helpText: 'This is a sample help message',
+        name:'Mr. Robot'
     })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 50,
-        location: 'Philadelphia'
+        location: 'Philadelphia',
+        name: 'Mr. Robot'
     })
 })
 
-app.get('/')
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
