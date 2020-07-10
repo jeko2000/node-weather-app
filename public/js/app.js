c738888bb476013db808c4d4dc85efac988a643d
Johnny Ruiz <jeko2000@yandex.com>
2020-07-09 23:14:39 -0400

    Complete 57. Browser HTTP Requests with Fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side JavaScript file loaded')
+
+var address = 'Boston';
+
+fetch('http://localhost:3000/weather?address=' + address).then(response => {
+    response.json().then(data => {
+        if (data.error) {
+            return console.log(data.error);
+        }
+        console.log(data);
+    });
+})
